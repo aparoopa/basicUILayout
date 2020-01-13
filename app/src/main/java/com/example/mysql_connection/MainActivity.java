e4962396dd723e41471712ee497b722ef966bd21
De$tro <supriyo.jobs91@gmail.com>
2020-01-13 22:00:11 +0530

    App

---
+++ app/src/main/java/com/example/mysql_connection/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.mysql_connection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+    EditText UsernameEt,PasswordEt; GoogleSignInClient mGoogleSignInClient; SignInButton signin;
+    int RC_SIGN_IN= 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        UsernameEt= (EditText)findViewById(R.id.etUserName);
+        PasswordEt= (EditText) findViewById(R.id.etPassword);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        signin = findViewById(R.id.sign_in_button);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                    // ...
+                }
+            }
+        });
+
+    }
+
+    public void OnLogin(View view)
+    {
+        String username= UsernameEt.getText().toString();
+        String password= PasswordEt.getText().toString();
+        String type= "login";
+        BackgroundWorker backgroundWorker= new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
+
+    }
+    private void signIn()
+    {
+        Intent signInIntent= mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+           Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+           startActivity(intent);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w( "Error", "signInResult:failed code=" + e.getStatusCode());
+
+        }
+    }
+}
